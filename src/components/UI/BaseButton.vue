f780043bf20660e952cd4fcfb97faf8dc62539fb
Alex Franco <alex.franco9@icloud.com>
2021-12-01 22:59:04 -0600

    initial commit

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode'],
+};
+</script>
+
+<style scope>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #1e317d;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #1e317d69;
+}
+</style>
